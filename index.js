91359d88f6b0c66285ca1732d1f46140f21e8764
Matej Teo Zilak <teo@teo.sk>
2015-02-12 17:21:25 +0100

    finished migration script

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var s3helper = require('./s3helper'),
     _ = require('lodash'),
     fs = require('fs');
 
-// s3.listBuckets(function(err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else     console.log(data);           // successful response
-// });
 
+console.time("migration");
 
 s3helper.listAllKeys('').then(function(keyArr) {
     var flattenedObjects = _.flatten(_.union(keyArr));
@@ -46,7 +43,7 @@ s3helper.listAllKeys('').then(function(keyArr) {
         if(err) {
             console.log(err);
         } else {
-            console.log("The file was saved!");
+            console.log("The file with groups including info about duplicates was saved! Check output/groups.txt");
         }
     }); 
 
@@ -68,47 +65,15 @@ s3helper.listAllKeys('').then(function(keyArr) {
         if(err) {
             console.log(err);
         } else {
-            console.log("The file was saved!");
+            console.log("The file with list of duplicates was saved! Check output/dupes.txt");
         }
-    });     
-});
+    });
 
-// var params = {
-//   Bucket: config.buckets.targetBucket, /* required */
-//   CopySource: config.buckets.sourceBucket + '/abcd/hell_1.png', /* required */
-//   Key: 'hell_1.png', /* required */
-//   // ACL: 'private | public-read | public-read-write | authenticated-read | bucket-owner-read | bucket-owner-full-control',
-//   // CacheControl: 'STRING_VALUE',
-//   // ContentDisposition: 'STRING_VALUE',
-//   // ContentEncoding: 'STRING_VALUE',
-//   // ContentLanguage: 'STRING_VALUE',
-//   // ContentType: 'STRING_VALUE',
-//   // CopySourceIfMatch: 'STRING_VALUE',
-//   // CopySourceIfModifiedSince: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
-//   // CopySourceIfNoneMatch: 'STRING_VALUE',
-//   // CopySourceIfUnmodifiedSince: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
-//   // CopySourceSSECustomerAlgorithm: 'STRING_VALUE',
-//   // CopySourceSSECustomerKey: 'STRING_VALUE',
-//   // CopySourceSSECustomerKeyMD5: 'STRING_VALUE',
-//   // Expires: new Date || 'Wed Dec 31 1969 16:00:00 GMT-0800 (PST)' || 123456789,
-//   // GrantFullControl: 'STRING_VALUE',
-//   // GrantRead: 'STRING_VALUE',
-//   // GrantReadACP: 'STRING_VALUE',
-//   // GrantWriteACP: 'STRING_VALUE',
-//   // Metadata: {
-//   //   someKey: 'STRING_VALUE',
-//   //   /* anotherKey: ... */
-//   // },
-//   // MetadataDirective: 'COPY | REPLACE',
-//   // SSECustomerAlgorithm: 'STRING_VALUE',
-//   // SSECustomerKey: 'STRING_VALUE',
-//   // SSECustomerKeyMD5: 'STRING_VALUE',
-//   // SSEKMSKeyId: 'STRING_VALUE',
-//   // ServerSideEncryption: 'AES256',
-//   // StorageClass: 'STANDARD | REDUCED_REDUNDANCY',
-//   // WebsiteRedirectLocation: 'STRING_VALUE'
-// };
-// s3.copyObject(params, function(err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else     console.log(data);           // successful response
-// });
+    //let's copy.
+    s3helper.copyObjects(dupeFreeObjects).then(function(msg) {
+        console.log(msg);
+        console.timeEnd("migration");
+    }, function(msg) {
+        console.log(msg);
+    });
+});
--- s3helper.js
+++ s3helper.js
@@ -1,17 +1,20 @@
 var AWS = require('aws-sdk'),
     Q = require('q'),
+    _ = require('lodash'),
     config = require('./config').config;
 
-AWS.config.update(config.amazon);
+AWS.config.update(config.amazon_west);
+var s3_west = new AWS.S3(); 
 
-var s3 = new AWS.S3(); 
+AWS.config.update(config.amazon_central);
+var s3_central = new AWS.S3(); 
 
 exports.listAllKeys = function (marker, deferred, allKeys) {
     if (!deferred) {
         var allKeys = [],
             deferred = Q.defer();
     }
-    s3.listObjects({Bucket: config.buckets.sourceBucket, Marker: marker/*, MaxKeys: 3*/}, function(err, data){
+    s3_central.listObjects({Bucket: config.buckets.sourceBucket, Marker: marker/*, MaxKeys: 3*/}, function(err, data){
       if (err) console.log(err);
       allKeys.push(data.Contents);
 
@@ -19,6 +22,42 @@ exports.listAllKeys = function (marker, deferred, allKeys) {
         exports.listAllKeys(data.Contents.slice(-1)[0].Key, deferred, allKeys);
       else
         deferred.resolve(allKeys);
+    });
+
+    return deferred.promise;
+}
+
+exports.copyObjects = function(objects) {
+    var deferred = Q.defer(),
+        params = {
+            Bucket: config.buckets.targetBucket
+        },
+        keys = Object.keys(objects),
+        len = keys.length,
+        filesProcessed = 0,
+        filesSuccessfullyProcessed = 0;
+
+    keys.forEach(function(key, i) {
+        var obj = objects[key];
+
+        params.CopySource = config.buckets.sourceBucket + '/' + obj.Key,
+        params.Key = _.last(obj.Key.split('/'));
+
+        s3_west.copyObject(params, function(err, data) {
+            filesProcessed++;
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                filesSuccessfullyProcessed++;
+            }
+            if (filesProcessed == len) {
+                if (filesProcessed == filesSuccessfullyProcessed) {
+                    deferred.resolve('success');
+                } else {
+                    deferred.reject('fail');
+                }
+            }
+        });
     });
 
     return deferred.promise;
